f8d1c49d2342958843804770185be4c606f86afd
eduardopozenatto <carlospozenatto.contato@gmail.com>
2025-10-24 08:32:32 -0300

    Update Reproduction.tsx

--- src/pages/Reproduction.tsx
+++ src/pages/Reproduction.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import lifeCycle from "@/assets/life_cycle.jpg";
+import lifeCycle from "@/assets/life_cycle.jpeg";
 import { Heart, Users, RefreshCw, Sparkles } from "lucide-react";
 
 export const Reproduction = () => {
